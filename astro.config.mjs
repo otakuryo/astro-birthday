e276bf4acc6598e1a1f6a0d6698a56a055a95f34
agustin <agustin.ryo@gmail.com>
2023-03-19 19:00:09 +0100

    feat-first-version

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  server: {
+    host: true
+  },
+  integrations: [tailwind()],
+  site: 'https://otakuryo.github.io'
+});
